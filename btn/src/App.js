918a9bda7863db643ed76707891932c0841c6a3a
Shyam-hinsu <shyamhinsu000@gmail.com>
2021-07-22 21:42:20 +0530

    login_page

---
+++ btn/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+
+const App= () => {
+
+    const [fullname,setfullName]=useState({
+            fname: "",
+            lname: "",
+            email:"",
+            mobileNo:"",
+    });
+
+
+    const inputEvent = (event) =>{
+        console.log(event.target.value);
+        
+        const value = event.target.value;
+        const name = event.target.name;
+
+        setfullName((preValue)=>{
+            if(name === "fName"){ 
+                    return{
+                        fname : value,
+                        lname : preValue.lname,
+                        email: preValue.email,
+                        mobileNo : preValue. mobileNo,
+                    };
+                   
+                } else  if(name === "lName"){ 
+                    return{
+                        fname :preValue.fname,
+                        lname : value,
+                        email: preValue.email,
+                        mobileNo : preValue. mobileNo,
+                    };
+                 }else  if(name === "Email"){ 
+                    return{
+                        fname :preValue.fname,
+                        lname : preValue.lname,
+                        email: value,
+                        mobileNo : preValue. mobileNo,
+                    };
+                 }else  if(name === "MobNo"){ 
+                    return{
+                        fname :preValue.fname,
+                        lname : preValue.lname,
+                        email: preValue.email,
+                        mobileNo : value,
+                    };
+                 }
+                 
+            })
+        }
+    const fullNameSubmit=(e)=>{
+            e.preventDefault();
+            alert("submited")
+     
+    }
+  
+
+    return(
+
+        <>
+        <form onSubmit={fullNameSubmit}>
+        <div>
+        <h1>😊 Hello {fullname.fname} {fullname.lname}  😊</h1>
+        <h1>   {fullname.email}  </h1>
+        <h1>    {fullname.mobileNo} </h1>
+        <input  type="text" placeholder="Your Name" className="box" onChange={inputEvent} value={fullname.fname} name='fName'/>
+        <input  type="text" placeholder="Your LastName" className="box" onChange={inputEvent} value={fullname.lname} name='lName'/>
+        <input  type="email" placeholder="Your email" className="box" onChange={inputEvent} value={fullname.email} name='Email'/>
+        <input  type="number" placeholder="Your mobile number" className="box" onChange={inputEvent} value={fullname.mobileNo} name='MobNo' max="10"/>
+        <button type="submit" >Click Here</button>
+        </div>
+        </form>
+        </>
+    );
+}
+   
+export default App;
